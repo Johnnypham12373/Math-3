e12215c28a0895630c389c7c78b0e3db1ccba1b5
Johnnypham12373 <huypham12373@gmail.com>
2021-06-07 01:29:15 -0700

    Image update

---
+++ website.js
@@ -0,0 +1,31 @@
+var modal12 = document.getElementById("myModal12");
+
+var img12 = document.getElementById("myImg12");
+var captionText12 = document.getElementById("caption12");
+img12.onclick = function(){
+  modal12.style.display = "block";
+  modalImg12.src = this.src
+  captionText.innerHTML = this.alt;
+}
+
+var span12 = document.getElementsByClassName("close12")[0];
+
+span12.onclick = function() { 
+  modal12.style.display = "none";
+}
+
+var modal13 = document.getElementById("myModal13");
+
+var img13 = document.getElementById("myImg13");
+var captionText13 = document.getElementById("caption13");
+img13.onclick = function(){
+  modal13.style.display = "block";
+  modalImg13.src = this.src
+  captionText.innerHTML = this.alt;
+}
+
+var span13 = document.getElementsByClassName("close13")[0];
+
+span13.onclick = function() { 
+  modal13.style.display = "none";
+}
